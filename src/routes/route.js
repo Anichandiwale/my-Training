818df56659d36f6e86cd236f5af71b1d1d6376af
Your Name <you@example.com>
2022-08-12 09:57:49 +0530

    i have completed problem2

--- src/routes/route.js
+++ src/routes/route.js
@@ -55,6 +55,76 @@ router.post('/players', function (req, res) {
        
 
 
+})//PROBLEM II:
+// you will be given an array of persons (1.e an array of objects)..each person will have a 
+//(name: String, age: Number, yatingStatus: true/false(Boolean)} take input in query param as votingAge..
+//and for all the people above that age, change votingStatus as true also return an array consisting of only
+// the person that can vote
+
+// WRITE A POST API TO THE ABOVE
+
+// take this as sample for array of persons: 
+let persons = [
+
+    {
+        name: "PK",
+
+        age: 10,
+        
+        votingStatus: false
+    },
+
+    {
+        name: "SK",
+
+        age: 20,
+
+        votingStatus: false
+    },
+
+    {
+        name: "AA",
+        
+        age: 70,
+
+        votingStatus: false
+    },
+
+    {
+        name: "SC",
+
+        age: 5, 
+        
+        votingStatus: false
+    },
+
+    {
+        name: "HO",
+
+        age: 40,
+
+        votingStatus: false
+    }
+]
+
+router.post('/persons', function (req, res) {
+
+    let input = req.query.input;
+    let newperson=[];
+    for (let i = 0; i < persons.length; i++) {
+        if (persons[i].age >= input) {
+
+            persons[i].votingStatus = true;
+            newperson.push(persons[i])
+        }
+    }
+    if (newperson.length == 0) {
+        return res.send("dose'nt exist data")
+    } else {
+        return res.send(newperson);
+    }
 })
+
+
 
 module.exports = router;
